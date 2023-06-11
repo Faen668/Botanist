62b9b56c759d64a09c499f8f1004502fa6d274ec
K1ngTr4cker <Christian.Daniels901@gmail.com>
2023-06-12 00:03:26 +0100

    Restored proper sleep time in loop

--- Scripts/create-release.bat
+++ Scripts/create-release.bat
@@ -4,49 +4,51 @@ call encode-csv-strings.bat
 ::Delete 'Release' folder and create a new one.
 rmdir "%modpath%\release" /s /q
 mkdir "%modpath%\release"
+mkdir "%modpath%\release\loose"
+mkdir "%modpath%\release\packed"
 
 ::Compile and copy over all local .ws scripts.
-mkdir "%modpath%\release\mods\mod%modname%\content\scripts\local\
-> "%modpath%\release\mods\mod%modname%\content\scripts\local\Botanist_Compiled.ws" (for /r "%modpath%\Source\local" %%F in (*.ws) do @type "%%F")
+mkdir "%modpath%\release\loose\mods\mod%modname%\content\scripts\local\
+> "%modpath%\release\loose\mods\mod%modname%\content\scripts\local\Botanist_Compiled.ws" (for /r "%modpath%\Source\local" %%F in (*.ws) do @type "%%F")
 
 ::Copy over the compiled scripts .ws scripts.
-XCOPY "%modpath%\release\mods\mod%modname%\content\scripts\local\" "%modpath%\Compiled" /e /s /y
+XCOPY "%modpath%\release\loose\mods\mod%modname%\content\scripts\local\" "%modpath%\Compiled" /e /s /y
 
 ::Copy over all vanilla .ws scripts.
-XCOPY "%modpath%\Source\game" "%modpath%\release\mods\mod%modname%\content\Scripts\game\" /e /s /y
+XCOPY "%modpath%\Source\game" "%modpath%\release\loose\mods\mod%modname%\content\Scripts\game\" /e /s /y
 
 ::Copy over all primer patch .ws scripts.
-XCOPY "%modpath%\primer\game" "%modpath%\release\PrimerPatch\mods\mod%modname%\content\Scripts\game\" /e /s /y
+XCOPY "%modpath%\primer\game" "%modpath%\release\loose\PrimerPatch\mods\mod%modname%\content\Scripts\game\" /e /s /y
 
 ::Copy over the menu xml.
-mkdir "%modpath%\release\bin\config\r4game\user_config_matrix\pc\"
-XCOPY "%modPath%\Menu" "%modpath%\release\bin\config\r4game\user_config_matrix\pc\" /e /s /y
+mkdir "%modpath%\release\loose\bin\config\r4game\user_config_matrix\pc\"
+XCOPY "%modPath%\Menu" "%modpath%\release\loose\bin\config\r4game\user_config_matrix\pc\" /e /s /y
 
 ::Copy over the encoded strings.
-XCOPY "%modpath%\Strings\" "%modpath%\release\mods\mod%modname%\content\" /e /s /y
+XCOPY "%modpath%\Strings\" "%modpath%\release\loose\mods\mod%modname%\content\" /e /s /y
 
 ::Copy over the DLC bundle files.
-XCOPY "%modpath%\Build\Botanist\packed\DLC\dlc_botanist\content" "%modpath%\release\dlc\dlc%modname%\content\" /e /s /y
+XCOPY "%modpath%\Build\Botanist\packed\DLC\dlc_botanist\content" "%modpath%\release\loose\dlc\dlc%modname%\content\" /e /s /y
 
 ::Copy over the Installation Instructions.
-XCOPY "%modpath%\Instructions\Installation Instructions.txt" "%modpath%\release\"
-XCOPY "%modpath%\Instructions\Primer\Installation Instructions.txt" "%modpath%\release\PrimerPatch"
+XCOPY "%modpath%\Instructions\Installation Instructions.txt" "%modpath%\release\loose\"
+XCOPY "%modpath%\Instructions\Primer\Installation Instructions.txt" "%modpath%\release\loose\PrimerPatch"
 
 ::Copy over the Shared Util dependencies
-XCOPY "%supath%\mod_sharedutils_mappins\" "%modpath%\release\mods\mod_sharedutils_mappins\" /e /s /y
-XCOPY "%supath%\mod_sharedutils_oneliners\" "%modpath%\release\mods\mod_sharedutils_oneliners\" /e /s /y
-XCOPY "%supath%\mod_sharedutils_helpers\" "%modpath%\release\mods\mod_sharedutils_helpers\" /e /s /y
-XCOPY "%supath%\mod_sharedutils_tiny_bootstrapper\" "%modpath%\release\mods\mod_sharedutils_tiny_bootstrapper\" /e /s /y
-XCOPY "%supath%\mod_sharedutils_storage\" "%modpath%\release\mods\mod_sharedutils_storage\" /e /s /y
+XCOPY "%supath%\mod_sharedutils_mappins\" "%modpath%\release\loose\mods\mod_sharedutils_mappins\" /e /s /y
+XCOPY "%supath%\mod_sharedutils_oneliners\" "%modpath%\release\loose\mods\mod_sharedutils_oneliners\" /e /s /y
+XCOPY "%supath%\mod_sharedutils_helpers\" "%modpath%\release\loose\mods\mod_sharedutils_helpers\" /e /s /y
+XCOPY "%supath%\mod_sharedutils_tiny_bootstrapper\" "%modpath%\release\loose\mods\mod_sharedutils_tiny_bootstrapper\" /e /s /y
+XCOPY "%supath%\mod_sharedutils_storage\" "%modpath%\release\loose\mods\mod_sharedutils_storage\" /e /s /y
 
 ::Create zip file for the release.
-"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\Botanist.zip" "%modpath%\release\mods"
-"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\Botanist.zip" "%modpath%\release\dlc"
-"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\Botanist.zip" "%modpath%\release\bin"
-"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\Botanist.zip" "%modpath%\release\Installation Instructions.txt"
+"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\packed\Botanist.zip" "%modpath%\release\loose\mods"
+"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\packed\Botanist.zip" "%modpath%\release\loose\dlc"
+"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\packed\Botanist.zip" "%modpath%\release\loose\bin"
+"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\packed\Botanist.zip" "%modpath%\release\loose\Installation Instructions.txt"
 
-"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\PrimerPatch.zip" "%modpath%\release\PrimerPatch\mods"
-"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\PrimerPatch.zip" "%modpath%\release\PrimerPatch\Installation Instructions.txt"
+"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\packed\PrimerPatch.zip" "%modpath%\release\loose\PrimerPatch\mods"
+"C:\Program Files\7-Zip\7z.exe" a "%modpath%\release\packed\PrimerPatch.zip" "%modpath%\release\loose\PrimerPatch\Installation Instructions.txt"
 
-del "%modpath%\release\Installation Instructions.txt"
-del "%modpath%\release\PrimerPatch\Installation Instructions.txt"
+del "%modpath%\release\loose\Installation Instructions.txt"
+del "%modpath%\release\loose\PrimerPatch\Installation Instructions.txt"
--- Source/local/Botanist_UIRenderLoop.ws
+++ Source/local/Botanist_UIRenderLoop.ws
@@ -123,7 +123,7 @@ state on_tick in Botanist_UIRenderLoop
 			old_herb_requirements = new_herb_requirements;
 			
 			//BT_Logger("UI Update Loop took: " + (theGame.GetLocalTimeAsMilliseconds() - stt) + " milliseconds to run");
-			SleepOneFrame();
+			Sleep(2);
 		}
 	}
 
